4e0123e5ac30016a3ac802de6f4c095ae3ead50b
Jgfsun <1587884826@qq.com>
2021-05-20 23:09:45 +0800

    change the py file name

--- eeg_ui_test.py
+++ eeg_ui_test.py
@@ -1,12 +1,15 @@
 # -*-coding:utf-8-*-
 from PyQt5.QtCore import *
-from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 import numpy as np
 from ui4 import Ui_form
 import pyqtgraph as pg
+from utils import epoch_buffer
+from inlet import save_data
+import threading
 
+moved_length = 2
 
 class MainDialogImgBW(QMainWindow, Ui_form):
     def __init__(self):
@@ -15,6 +18,7 @@ class MainDialogImgBW(QMainWindow, Ui_form):
         self.setWindowTitle("Sleep Stage Scoring")
         # self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
         self.i = 0
+        self.j = 0
         self.N = 200
         self.stage = 4
         self.dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
@@ -36,25 +40,20 @@ class MainDialogImgBW(QMainWindow, Ui_form):
         self.init_RW()
         self.init_FFT()
 
-        data = np.load("./eeg_data.npz", allow_pickle=True)
-        self.x = data['x']
-        self.y = data['y']
-        self.sampling_rate = int(data['fs'])
-
         self.init_timer()
 
     def init_timer(self):
         self.timer_re = QTimer(self)
-        self.timer_re.start(10)  # ms
+        self.timer_re.start(20)  # ms
         self.timer_re.timeout.connect(self.plotData)
 
     def init_F(self):
-        set_left = pg.AxisItem('left', pen='y',  maxTickLength=-5)
-        set_bottom = pg.AxisItem('bottom', pen='y',  maxTickLength=-5)
+        set_left = pg.AxisItem('left', pen='y', maxTickLength=-5)
+        set_bottom = pg.AxisItem('bottom', pen='y', maxTickLength=-5)
         p = self.F.addPlot(row=0, col=0, axisItems={'left': set_left, 'bottom': set_bottom})  # 新建一个子图
         # p2 = self.F.addPlot(row=1, col=0)
         # p.showGrid(x=True, y=True)
-        p.setRange(yRange=(-100, 100), xRange=(10, 1000), disableAutoRange=True)  # 设置坐标轴刻度范围
+        p.setRange(yRange=(-150, 150), xRange=(10, 380), disableAutoRange=True)  # 设置坐标轴刻度范围
         p.addLine(y=0, pen=(255, 255, 0))  # label='Frequence(Hz)'
         p.hideAxis('bottom')
         p.setLabel(axis='left', text='Amplitude(uV)', color='yellow')
@@ -64,11 +63,11 @@ class MainDialogImgBW(QMainWindow, Ui_form):
         self.curve1 = p.plot(pen="r", name="channel 1")
 
     def init_RW(self):
-        set_left = pg.AxisItem('left', pen='y',  maxTickLength=-5)
-        set_bottom = pg.AxisItem('bottom', pen='y',maxTickLength=-5)
+        set_left = pg.AxisItem('left', pen='y', maxTickLength=-5)
+        set_bottom = pg.AxisItem('bottom', pen='y',  maxTickLength=-5)
         p2 = self.RW.addPlot(row=0, col=0, axisItems={'left': set_left, 'bottom': set_bottom})  # 新建一个子图
         # p2.showGrid(x=True, y=True)
-        p2.setRange(yRange=(-100, 100), xRange=(10, 1000), disableAutoRange=True)  # 设置坐标轴刻度范围
+        p2.setRange(yRange=(-50, 50), xRange=(10, 380), disableAutoRange=True)  # 设置坐标轴刻度范围
         p2.addLine(y=0, pen=(255, 255, 0))
         p2.hideAxis('bottom')
         p2.setLabel(axis='left', text='Amplitude(uV)', color='yellow')
@@ -76,11 +75,11 @@ class MainDialogImgBW(QMainWindow, Ui_form):
         # p.setTitle('EEG', color='r', fontsize=20,)
         p2.addLegend(offset=(10, 10))  # 添加label,并设置位置
         self.curve3 = p2.plot(pen="r", name="seta")
-        self.curve4 = p2.plot(pen='b', name='alpha')
+        self.curve4 = p2.plot(pen='y', name='alpha')
 
     def init_FFT(self):
-        set_left = pg.AxisItem('left', pen='y',  maxTickLength=-5)
-        set_bottom = pg.AxisItem('bottom', pen='y',maxTickLength=-5)
+        set_left = pg.AxisItem('left', pen='y', maxTickLength=-5)
+        set_bottom = pg.AxisItem('bottom', pen='y', maxTickLength=-5)
         p3 = self.FFT.addPlot(row=0, col=0, axisItems={'left': set_left, 'bottom': set_bottom})  # 新建一个子图
         # p2.showGrid(x=True, y=True)
         p3.setRange(yRange=(-0, 10), xRange=(0, 100), disableAutoRange=True)  # 设置坐标轴刻度范围
@@ -91,22 +90,29 @@ class MainDialogImgBW(QMainWindow, Ui_form):
         self.curve5 = p3.plot(pen="r", name="channel 1")
 
     def plotData(self):
-        data_x = self.x[self.i * 1: 400 + self.i * 1]
-        self.curve1.setData(data_x[::-1])
-        self.curve3.setData(data_x)
-        self.curve4.setData(data_x[::-1])
+        if epoch_buffer.get_data_state(self.i, moved_length):  #
+        # try:try这个方法再nx上不行，但是再nano和windows上可以
+            x, sample_rate = epoch_buffer.get_data(400, moved_length, 400/moved_length, self.i)
+            self.curve1.setData(x)
 
-        x = self.N / 2 * np.linspace(0, 1, self.N / 2)
-        transformed = 1.0 / self.N * np.abs(np.fft.fft(data_x, self.N))
-        self.curve5.setData(x, transformed[0:int(self.N / 2)], )
+            seta_x = epoch_buffer.get_filter_rhythm(4, 8)
+            alpha_x = epoch_buffer.get_filter_rhythm(8, 13)
+            self.curve3.setData(seta_x)
+            self.curve4.setData(alpha_x)
 
-        label = (self.i + 10) // 300
-        self.stage = self.y[label]
-        if (self.i + 10) % 300 == 0:
-            self.dict[self.stage] = self.dict[self.stage] + 1
-        # 显示分期结果
-        self.classes(self.stage, self.sampling_rate)
-        self.i = self.i + 1
+            x_ = self.N / 2 * np.linspace(0, 1, int(self.N / 2))
+            transformed = 1.0 / self.N * np.abs(np.fft.fft(x, self.N))
+            self.curve5.setData(x_, transformed[0:int(self.N / 2)], )
+
+            # classes
+            if epoch_buffer.get_label_state(self.j):
+                self.stage = epoch_buffer.get_label(self.j)
+                self.dict[self.stage] = self.dict[self.stage] + 1
+                self.classes(self.stage, sample_rate)
+                self.j = self.j + 1
+            self.i = self.i + 1
+        # except:
+        #     self.i = 0
 
     def classes(self, stage, sampling_rate):
         self.lineEdit.setText(str(self.dict[0]))
@@ -120,9 +126,12 @@ class MainDialogImgBW(QMainWindow, Ui_form):
 
 
 if __name__ == "__main__":
+    thread_save = threading.Thread(target=save_data)
+    thread_save.start()
     app = QApplication(sys.argv)
     main = MainDialogImgBW()
     main.show()
     # main.showFullScreen()
     app.installEventFilter(main)
     sys.exit(app.exec_())
+
